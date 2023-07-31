9d9eaad73c8e12d7f89ad17543551319aa177d43
rummolprod999 <rummolprod999@gmail.com>
2023-07-31 06:43:44 +0300

    new

--- src/main/java/parser/parsers/ParserVipAst.kt
+++ src/main/java/parser/parsers/ParserVipAst.kt
@@ -1,8 +1,5 @@
 package parser.parsers
 
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
-import java.util.logging.Level
 import org.openqa.selenium.By
 import org.openqa.selenium.JavascriptExecutor
 import org.openqa.selenium.WebElement
@@ -13,6 +10,9 @@ import org.openqa.selenium.support.ui.WebDriverWait
 import parser.extensions.findElementWithoutException
 import parser.logger.logger
 import parser.tenders.TenderVipAst
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import java.util.logging.Level
 
 class ParserVipAst : IParser, ParserAbstract() {
 
@@ -52,7 +52,7 @@ class ParserVipAst : IParser, ParserAbstract() {
 
     private fun parserSelen() {
         val options = ChromeOptions()
-        options.addArguments("headless")
+        //options.addArguments("headless")
         options.addArguments("disable-gpu")
         options.addArguments("no-sandbox")
         drv = ChromeDriver(options)
